6f9fe6f2cf8bbdb3a01690e9d1aa8faf1f7e029f
Yevgen <yevgen.yelik@gmail.com>
2020-12-14 00:18:56 +0200

    URL=HOST + '...', delete two bottom str

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import json
 
 HOST = 'https://www.avforums.com'
-URL = 'https://www.avforums.com/threads/samsung-q80t-qe55q80t-review-comments.2328417/'  # parsed page url
+URL = HOST + '/threads/samsung-q80t-qe55q80t-review-comments.2328417/'
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15'
@@ -13,6 +13,7 @@ HEADERS = {
 def get_html(url, params=None):
     response = requests.get(url, params=params, headers=HEADERS)
     return response
+
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -33,8 +34,4 @@ def get_content(html):
     with open('content.json', 'w') as file:
         json.dump(parsed_data, file, indent=2)
 
-    return parsed_data
-
-html = get_html(URL)
-print(get_content(html.text))
-
+    return
